1d31a8db8e4c1d5d1b8342505118bedfeb555235
Shreya Jaisal <shreyajaisal90@gmail.com>
2024-11-19 14:29:13 +0530

    Add files via upload

---
+++ config/config.py
@@ -0,0 +1,4 @@
+'''class Config:
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:12345678@localhost/social_media_app'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True  '''
