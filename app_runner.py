61be12a986188ff946a3e22bf34e37ceeb3fc456
Shreya Jaisal <shreyajaisal90@gmail.com>
2024-11-22 22:55:02 +0530

    Update app_runner.py

--- app_runner.py
+++ app_runner.py
@@ -2,15 +2,15 @@ import subprocess
 
 # Run the service corresponding to the user_app
 def run_user_service():
-    subprocess.Popen(["/Users/shreyajaisal/Desktop/social_media_app/venv/bin/python3", "user_app/app.py"])
+    subprocess.Popen(["<version/of/Python/used/within/the/venv>", "user_app/app.py"])
 
 # Run the service corresponding to the post_app
 def run_post_service():
-    subprocess.Popen(["/Users/shreyajaisal/Desktop/social_media_app/venv/bin/python3", "post_app/app.py"])
+    subprocess.Popen(["<version/of/Python/used/within/the/venv>", "post_app/app.py"])
 
 # Run the service corresponding to the shared messaging app
 def run_shared_service():
-    subprocess.Popen(["/Users/shreyajaisal/Desktop/social_media_app/venv/bin/python3", "message_app/app.py"])
+    subprocess.Popen(["<version/of/Python/used/within/the/venv>", "message_app/app.py"])
 
 if __name__ == '__main__':
     run_user_service()
